8af54386436f7700154b23b528a5b154ddbc3d84
Jim Szalewski <jim.szalewski@gmail.com>
2021-07-03 15:03:24 -0600

    create searches action and route

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,8 +1,19 @@
 class Api::V1::SearchesController < ApplicationController
-    def index
-        searches = Search.all
-        # byebug
+  def index
+    searches = Search.all
 
-        render json: searches, status: 200
+    render json: searches, status: 200
+  end
+
+  def create
+    search = Search.create!(criteria: dish_params[:criteria], url: dish_params[:url])
+
+    render json: search, status: 200
+  end
+
+  private
+
+    def dish_params
+      params.require(:dish).permit(:criteria, :url)
     end
 end
--- spec/requests/api/v1/searches_spec.rb
+++ spec/requests/api/v1/searches_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Searches", type: :request do
-  describe "GET /index" do
-    it "returns all searches" do
-      search_1 = create(:search, criteria: "Fruit Tart")
-      search_2 = create(:search, criteria: "Tiramisu")
-      search_3 = create(:search, criteria: "Spaghetti Bolognese")
+RSpec.describe 'Api::V1::Searches', type: :request do
+  describe 'GET /index' do
+    it 'returns all searches' do
+      search_1 = create(:search, criteria: 'Fruit Tart')
+      search_2 = create(:search, criteria: 'Tiramisu')
+      search_3 = create(:search, criteria: 'Spaghetti Bolognese')
 
       get '/api/v1/searches'
       results = JSON.parse(response.body)
@@ -17,6 +17,16 @@ RSpec.describe "Api::V1::Searches", type: :request do
       expect(results[1]['url']).to eq(search_2.url) 
       expect(results[2]['criteria']).to eq(search_3.criteria) 
       expect(results[2]['url']).to eq(search_3.url)  
+    end
+  end
+
+  describe 'POST /create' do
+    it 'creates a new search' do
+      req = { dish: { criteria: 'Gnocchi', url: 'https://api.spoonacular.com/recipes/complexSearch?query=Gnocchi'}}
+      
+      expect(Search.count).to eq(0)
+      post '/api/v1/searches', params: req
+      expect(Search.count).to eq(1)
     end
   end
 end
